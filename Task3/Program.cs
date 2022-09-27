0e5643028567c04df5e7799ec776443581e9d284
Sergey <smtishkar@mail.ru>
2022-09-27 10:59:08 +0300

    Добавил Task3 работающий вариант

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -9,56 +9,51 @@
 
 
 Console.Clear();
-int columns1 = 4;
-int rows1 = 2;
-int columns2 = 2;
-int rows2 = 5;
+int columns1 = 2;
+int rows1 = 3;
+int columns2 = 3;
+int rows2 = 2;
+
 
 int[,] firstMatrix = GetArray(rows1, columns1, 1, 4);
 int[,] secondMatrix = GetArray(rows2, columns2, 1, 4);
-PrintArray(firstMatrix);
-Console.WriteLine();
+Console.WriteLine("Матрица №2");        // Для удобства отображения вывел сначала вторую матрицу
 PrintArray(secondMatrix);
 Console.WriteLine();
-int[,] thirddMatrix = MatrixProduction(firstMatrix, secondMatrix);
+Console.WriteLine("Матрица №1");
+PrintArray(firstMatrix);
 Console.WriteLine();
-PrintArray(thirddMatrix);
 
 
+if (rows1 != columns2)
+{
+    Console.WriteLine("Матрицы нельзя перемножить");
+}
+else
+{
+    int[,] thirddMatrix = MatrixProduction(firstMatrix, secondMatrix);
+    Console.WriteLine();
+    PrintArray(thirddMatrix);
+}
 
 int[,] MatrixProduction(int[,] array1, int[,] array2)
 {
-    int[,] arr = new int[rows2, columns1];
-    if (array1.GetLength(1) == array2.GetLength(0))
-    {
-        for (int i = 0; i < array1.GetLength(0); i++)
-        {
-            for (int j = 0; j < array2.GetLength(1); j++)
-            {
-                for (int k = 0; k < array2.GetLength(0); k++)
-                {
-                    // Console.Write(arr[i, j]);               
-                    arr[i, j] += array1[i, k] * array2[k, j];
+    int[,] arr = new int[rows1, columns2];
 
-                    Console.Write($"{array1[i, k]} ");
-                    // // Console.Write ($"{array1[i,k+1]} ");
-                    Console.Write($"{array2[k, j]} ");
-                    // Console.Write ($"{array2[k,j+1]} ");
-                    Console.WriteLine();
-                    // Console.WriteLine (arr[i,j]);
-                }
-                Console.WriteLine();
+    for (int i = 0; i < array2.GetLength(1); i++)
+    {
+        for (int j = 0; j < array2.GetLength(1); j++)
+        {
+            arr[i, j] = 0;
+            for (int k = 0; k < array1.GetLength(1); k++)
+            {          
+                arr[i, j] += array1[i, k] * array2[k, j];
             }
         }
-        
     }
-    else Console.WriteLine("Матрицы нельзя перемножить");
     return arr;
+
 }
-
-
-
-
 
 int[,] GetArray(int m, int n, int minValue, int maxValue)
 {
@@ -74,7 +69,6 @@ int[,] GetArray(int m, int n, int minValue, int maxValue)
     return arr;
 }
 
-
 void PrintArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
@@ -86,28 +80,3 @@ void PrintArray(int[,] array)
         Console.WriteLine();
     }
 }
-
-
-
-// int [,] MatrixProduction (int [,] array1, int [,] array2)
-// {
-//     int [,] arr = new int [rows1, columns2]; 
-//     for (int i = 0; i < columns2; i++)
-//     {
-//         for (int j = 0; j < rows1; j++)
-//         {
-
-//             arr[i, j] = array1[i,j] * array2[j,i] + array1[i+1,j] * array2[j, i+1];
-//             Console.Write ($"{array1[i,j]} ");
-//             Console.Write ($"{array1[i+1,j]} ");
-//             Console.Write ($"{array2[j,i]} ");
-//             Console.WriteLine ($"{array2[j, i+1]} ");
-//             // Console.Write ($"{array2[i,j]} ");
-//             // Console.WriteLine ($"{array2[i, j+1]} ");
-
-//             Console.WriteLine (arr[i,j]);
-//         }
-
-//     }
-//     return arr;
-// }
